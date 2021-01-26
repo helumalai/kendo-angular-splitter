f7186917eab7df0bb158e5860739d3918e6c8028
helumalai <helumalai@yahoo.com>
2021-01-26 00:06:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-frj8hw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-frj8hw)
---
+++ app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+        <kendo-splitter orientation="vertical" style="height: 340px;">
+
+          <kendo-splitter-pane>
+            <kendo-splitter>
+
+              <kendo-splitter-pane [collapsible]="true" size="30%">
+                <div class="pane-content">
+                  <h3>Inner splitter / left pane</h3>
+                  <p>Resizable and collapsible.</p>
+                </div>
+              </kendo-splitter-pane>
+
+              <kendo-splitter-pane>
+                <div class="pane-content">
+                  <h3>Inner splitter / center pane</h3>
+                  <p>Resizable only.</p>
+                </div>
+              </kendo-splitter-pane>
+
+              <kendo-splitter-pane [collapsible]="true" size="30%">
+                <div class="pane-content">
+                  <h3>Inner splitter / right pane</h3>
+                  <p>Resizable and collapsible.</p>
+                </div>
+              </kendo-splitter-pane>
+
+            </kendo-splitter>
+          </kendo-splitter-pane>
+
+          <kendo-splitter-pane size="100px">
+            <div class="pane-content">
+              <h3>Outer splitter / Middle pane</h3>
+              <p>Resizable only.</p>
+            </div>
+          </kendo-splitter-pane>
+
+          <kendo-splitter-pane [resizable]="false" size="100px">
+            <div class="pane-content">
+              <h3>Outer splitter / Bottom pane</h3>
+              <p>Non-resizable and non-collapsible.</p>
+            </div>
+          </kendo-splitter-pane>
+        </kendo-splitter>
+      `,
+  styles: [`
+        .pane-content { padding: 0 10px; }
+        h3 { font-size: 1.2em; margin: 10px 0; padding: 0; }
+        p { margin: 0; padding: 0; }
+    ` ]
+})
+export class AppComponent { }
